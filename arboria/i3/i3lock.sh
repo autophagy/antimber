20632a1c171433aa35e35c7b142c579984d88b13
Autophagy <mika@crate.io>
2017-09-11 13:03:50 +0200

    Add multimonitor support for i3lock script

--- arboria/i3/i3lock.sh
+++ arboria/i3/i3lock.sh
@@ -13,14 +13,41 @@ IMAGE_SAMPLE=~/.config/i3/tmp-2.png
 scrot $IMAGE
 convert $IMAGE -scale 10% -modulate 100,50 -scale 1000% $IMAGE
 
-sample=$(convert $IMAGE -gravity center -crop 250x250+0+0 -resize 1x1 -format "%[fx:luminance]" info:)
-result=$(echo "$sample > 0.5"|bc)
+monitors=$(xrandr -q | grep ' connected' | wc -l)
 
-if [ $result -eq 1 ]
+if [ $monitors -gt 1 ]
 then
-  convert $IMAGE $ICON_BLACK -gravity center -composite -matte $IMAGE
+  sample=$(convert $IMAGE -gravity center -crop 250x250-1280+0 -resize 1x1 -format "%[fx:luminance]" info:)
+  result=$(echo "$sample > 0.5"|bc)
+  if [ $result -eq 1 ]
+  then
+    ICON=$ICON_BLACK
+  else
+    ICON=$ICON_WHITE
+  fi
+
+  convert $IMAGE $ICON -gravity center -geometry -1280+0 -composite -matte $IMAGE
+
+  sample=$(convert $IMAGE -gravity center -crop 250x250+1280+0 -resize 1x1 -format "%[fx:luminance]" info:)
+  result=$(echo "$sample > 0.5"|bc)
+  if [ $result -eq 1 ]
+  then
+    ICON=$ICON_BLACK
+  else
+    ICON=$ICON_WHITE
+  fi
+
+  convert $IMAGE $ICON -gravity center -geometry +1280+0 -composite -matte $IMAGE
 else
-  convert $IMAGE $ICON_WHITE -gravity center -composite -matte $IMAGE
+  sample=$(convert $IMAGE -gravity center -crop 250x250+0+0 -resize 1x1 -format "%[fx:luminance]" info:)
+  result=$(echo "$sample > 0.5"|bc)
+
+  if [ $result -eq 1 ]
+  then
+    convert $IMAGE $ICON_BLACK -gravity center -composite -matte $IMAGE
+  else
+    convert $IMAGE $ICON_WHITE -gravity center -composite -matte $IMAGE
+  fi
 fi
 
 i3lock -i $IMAGE $COLORS $VERCOLORS $WRONGCOLORS $MISC
