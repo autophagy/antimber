321e723fa5c16de7c5251473df5a9718645b6670
Autophagy <mika@crate.io>
2019-10-23 14:26:49 +0200

    Fixes and tweaks for Lenovo X1 Carbon 7th gen

--- feowerscyte/i3/i3lock.sh
+++ feowerscyte/i3/i3lock.sh
@@ -4,60 +4,13 @@ VERCOLORS="--insidevercolor=d2d2d266 --ringvercolor=2d2d2dff"
 WRONGCOLORS="--insidewrongcolor=a8949466 --ringwrongcolor=a89494ff"
 MISC="--keyhlcolor=2d2d2dff --bshlcolor=a89494ff"
 
-IMAGE=~/.config/i3/lockscreen.png
+IMAGE=$(grep --only-matching --perl-regex "(?<=file\=).*" ~/.config/nitrogen/bg-saved.cfg)
 
 #!/bin/bash
 
 #Constants
-DISPLAY_RE="([0-9]+)x([0-9]+)\\+([0-9]+)\\+([0-9]+)" # Regex to find display dimensions
-
-MD5_BKG_IMG=$(md5sum $IMAGE | cut -c 1-10)
-MD5_SCREEN_CONFIG=$(xrandr | md5sum - | cut -c 1-32) # Hash of xrandr output
-OUTPUT_IMG=~/.config/i3/"$MD5_SCREEN_CONFIG"."$MD5_BKG_IMG".png # Path of final image
-OUTPUT_IMG_WIDTH=0 # Decide size to cover all screens
-OUTPUT_IMG_HEIGHT=0 # Decide size to cover all screens
-
-#i3lock command
-LOCK_BASE_CMD="i3lock -i $OUTPUT_IMG $COLORS $VERCOLORS $WRONGCOLORS $MISC"
+LOCK_BASE_CMD="i3lock -i $IMAGE $COLORS $VERCOLORS $WRONGCOLORS $MISC"
 LOCK_CMD="$LOCK_BASE_CMD"
-
-if [ -e $OUTPUT_IMG ]
-then
-    # Lock screen since image already exists
-    $LOCK_CMD
-    exit 0
-fi
-
-#Execute xrandr to get information about the monitors:
-while read LINE
-do
-  #If we are reading the line that contains the position information:
-  if [[ $LINE =~ $DISPLAY_RE ]]; then
-    #Extract information and append some parameters to the ones that will be given to ImageMagick:
-    SCREEN_WIDTH=${BASH_REMATCH[1]}
-    SCREEN_HEIGHT=${BASH_REMATCH[2]}
-    SCREEN_X=${BASH_REMATCH[3]}
-    SCREEN_Y=${BASH_REMATCH[4]}
-
-    CACHE_IMG="$CACHE_FOLDER""$SCREEN_WIDTH"x"$SCREEN_HEIGHT"."$MD5_BKG_IMG".png
-    ## if cache for that screensize doesnt exist
-    if ! [ -e $CACHE_IMG ]
-    then
-  # Create image for that screensize
-        eval convert '$IMAGE' '-resize' '${SCREEN_WIDTH}X${SCREEN_HEIGHT}^' '-gravity' 'Center' '-crop' '${SCREEN_WIDTH}X${SCREEN_HEIGHT}+0+0' '+repage' '$CACHE_IMG'
-    fi
-
-    # Decide size of output image
-    if (( $OUTPUT_IMG_WIDTH < $SCREEN_WIDTH+$SCREEN_X )); then OUTPUT_IMG_WIDTH=$(($SCREEN_WIDTH+$SCREEN_X)); fi;
-    if (( $OUTPUT_IMG_HEIGHT < $SCREEN_HEIGHT+$SCREEN_Y )); then OUTPUT_IMG_HEIGHT=$(( $SCREEN_HEIGHT+$SCREEN_Y )); fi;
-
-    PARAMS="$PARAMS $CACHE_IMG -geometry +$SCREEN_X+$SCREEN_Y -composite "
-  fi
-done <<<"`xrandr`"
-
-#Execute ImageMagick:
-eval convert -size ${OUTPUT_IMG_WIDTH}x${OUTPUT_IMG_HEIGHT} 'xc:black' $OUTPUT_IMG
-eval convert $OUTPUT_IMG $PARAMS $OUTPUT_IMG
 
 #Lock the screen:
 $LOCK_CMD
