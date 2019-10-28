3291950396ed729bb694effa9957bd09f763c2b4
Autophagy <mika@crate.io>
2019-10-28 15:41:43 +0100

    Tweak audio keys, lockscreen

--- feowerscyte/i3/i3lock.sh
+++ feowerscyte/i3/i3lock.sh
@@ -4,13 +4,14 @@ VERCOLORS="--insidevercolor=d2d2d266 --ringvercolor=2d2d2dff"
 WRONGCOLORS="--insidewrongcolor=a8949466 --ringwrongcolor=a89494ff"
 MISC="--keyhlcolor=2d2d2dff --bshlcolor=a89494ff"
 
-IMAGE=$(grep --only-matching --perl-regex "(?<=file\=).*" ~/.config/nitrogen/bg-saved.cfg)
-
-#!/bin/bash
+IMAGE=/tmp/screen.png
+LOCK=$HOME/.config/i3/lock.png
+RES=$(xrandr | grep 'current' | sed -E 's/.*current\s([0-9]+)\sx\s([0-9]+).*/\1x\2/')
 
 #Constants
-LOCK_BASE_CMD="i3lock -i $IMAGE $COLORS $VERCOLORS $WRONGCOLORS $MISC"
+LOCK_BASE_CMD="i3lock -t -i $IMAGE $COLORS $VERCOLORS $WRONGCOLORS $MISC"
 LOCK_CMD="$LOCK_BASE_CMD"
 
-#Lock the screen:
+ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -filter_complex "boxblur=10" -vframes 1 $IMAGE -loglevel quiet
 $LOCK_CMD
+rm $IMAGE
