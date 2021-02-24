6c2e8af07b6f6d9e7b374ddd759e10799d5a41bb
Mika Naylor <mail@autophagy.io>
2021-02-24 11:37:46 +0100

    Add ctrl+g as passthrough leave command for qutebrowser

--- dotfiles/qutebrowser/config.py
+++ dotfiles/qutebrowser/config.py
@@ -143,4 +143,8 @@ c.qt.highdpi = True
 config.bind(',m', 'spawn -d mpv --ytdl-format="bestvideo[height<=?1080]+bestaudio/best" {url}')
 config.bind(',a', 'spawn -d mpv --no-video {url}')
 
+## Passthrough mode binding
+
+config.bind('<Ctrl-g>', 'mode-leave', mode='passthrough')
+
 config.load_autoconfig(False)
