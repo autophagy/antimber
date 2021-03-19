9711a840e73ac2195703074308d234b5683835b3
Mika Naylor <mail@autophagy.io>
2021-03-19 13:59:20 +0100

    Some xmobar tweaks, add xmonad to xinit

--- dotfiles/xmonad/xmonad.hs
+++ dotfiles/xmonad/xmonad.hs
@@ -33,7 +33,7 @@ myClickJustFocuses = False
 
 -- Width of the window border in pixels.
 --
-myBorderWidth = 2
+myBorderWidth = 1
 
 -- modMask lets you specify which modkey you want to use. The default
 -- is mod1Mask ("left alt").  You may also consider using mod3Mask
