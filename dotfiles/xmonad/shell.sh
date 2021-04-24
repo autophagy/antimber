ec347ad79320dd783397bed8d94f3002c23edf9e
Mika Naylor <mail@autophagy.io>
2021-04-24 12:42:56 +0200

    Default to opening shell at ~ if the inferred path is /usr/bin
    If opening a shell while over, say, Discord, I don't want it to open in the bin path

--- dotfiles/xmonad/shell.sh
+++ dotfiles/xmonad/shell.sh
@@ -7,20 +7,20 @@ CWD=''
 ID=$(xdpyinfo | grep focus | cut -f4 -d " ")
 
 # Get PID of process whose window this is
-PID=$(xprop -id $ID | grep -m 1 PID | cut -d " " -f 3)
+PID=$(xprop -id "$ID" | grep -m 1 PID | cut -d " " -f 3)
 
 # Get last child process (shell, vim, etc)
 if [ -n "$PID" ]; then
-  TREE=$(pstree -lpA $PID | tail -n 1)
-  PID=$(echo $TREE | awk -F'---' '{print $NF}' | sed -re 's/[^0-9]//g')
+  TREE=$(pstree -lpA "$PID" | tail -n 1)
+  PID=$(echo "$TREE" | awk -F'---' '{print $NF}' | sed -re 's/[^0-9]//g')
 
   # If we find the working directory, run the command in that directory
   if [ -e "/proc/$PID/cwd" ]; then
-    CWD=$(readlink /proc/$PID/cwd)
+    CWD=$(readlink "/proc/$PID/cwd")
   fi
 fi
-if [ -n "$CWD" ]; then
-  $CMD -cd $CWD
+if [ -n "${CWD}" ] && [ "${CWD}" != "/usr/bin" ]; then
+  $CMD -cd "$CWD"
 else
   $CMD
 fi
