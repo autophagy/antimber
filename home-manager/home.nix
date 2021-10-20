b945ae64ae54a374a07edd0705fe3917228cad79
Mika Naylor <mail@autophagy.io>
2021-10-20 20:42:50 +0200

    More packages...

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -7,4 +7,29 @@
   home.username = "mika";
   home.homeDirectory = "/home/mika";
   home.stateVersion = "21.05";
+
+  nixpkgs.config.allowUnfree = true;
+
+  home.packages = with pkgs; [
+    wget
+    bat
+    firefox
+    rxvt_unicode
+    keepassxc
+    dmenu
+    xmobar
+    pavucontrol
+    rofi
+    zoom-us
+    gimp
+    slack
+    kubectl kubectx
+    awscli2 azure-cli google-cloud-sdk
+    terraform
+    minikube
+    arandr
+    ripgrep
+    spotify
+    light
+  ];
 }
