9ab5e785588e1b38e2cb7d84ce3cac0b4ed0249c
Mika Naylor <mail@autophagy.io>
2021-10-22 19:51:42 +0200

    Add omz and syntax highlighting

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -20,6 +20,17 @@
       autoload -U promptinit; promptinit
       prompt pure
     '';
+    oh-my-zsh = {
+      enable = true;
+      plugins = ["ssh-agent" "git"];
+    };
+    plugins = [
+      {
+        name = "zsh-syntax-highlighting";
+        src = pkgs.zsh-syntax-highlighting;
+        file = "share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh";
+      }
+    ];
   };
 
   programs.fzf = {
