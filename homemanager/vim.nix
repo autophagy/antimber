54785be88a30e638c42255efd2dd327c9dea9dd5
Mika Naylor <mail@autophagy.io>
2022-07-08 18:15:27 +0200

    Remove custom build of coc, add as vim plugin

--- homemanager/vim.nix
+++ homemanager/vim.nix
@@ -10,18 +10,6 @@
     coc = {
       enable = true;
 
-      package = pkgs.vimUtils.buildVimPluginFrom2Nix {
-        pname = "coc.nvim";
-        version = "2022-05-21";
-        src = pkgs.fetchFromGitHub {
-          owner = "neoclide";
-          repo = "coc.nvim";
-          rev = "791c9f673b882768486450e73d8bda10e391401d";
-          sha256 = "sha256-MobgwhFQ1Ld7pFknsurSFAsN5v+vGbEFojTAYD/kI9c=";
-        };
-        meta.homepage = "https://github.com/neoclide/coc.nvim/";
-      };
-
       settings = {
         "languageserver" = {
           "nix" = {
@@ -188,6 +176,7 @@
     };
 
     plugins = with pkgs.vimPlugins; [
+      coc-nvim
       vim-trailing-whitespace
       nerdtree
       vim-fugitive
