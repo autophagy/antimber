32b26dc725e78f16693d0596564d8ed83c9884d1
Mika Naylor <mail@autophagy.io>
2022-07-29 00:22:24 +0200

    vim :: remove coc, use native lsp

--- home/editors/vim.nix
+++ home/editors/vim.nix
@@ -15,6 +15,48 @@
       # Completion/Linting/Highlighting
       vim-trailing-whitespace
       indentLine
+      cmp-buffer
+      cmp-nvim-lsp
+      cmp-path
+      luasnip
+      cmp_luasnip
+      lsp-status-nvim
+      {
+        plugin = nvim-cmp;
+        config = ''
+          set completeopt=menu,menuone,noselect
+
+          lua <<EOF
+            -- Setup nvim-cmp.
+            local cmp = require'cmp'
+
+            cmp.setup({
+              snippet = {
+                expand = function(args)
+                  require('luasnip').lsp_expand(args.body)
+                end,
+              },
+              window = {
+              },
+              mapping = cmp.mapping.preset.insert({
+                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                ['<C-Space>'] = cmp.mapping.complete(),
+                ['<C-e>'] = cmp.mapping.abort(),
+                ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                ['<Tab>'] = cmp.mapping.select_next_item(),
+                ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+              }),
+              sources = cmp.config.sources({
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' },
+                { name = 'buffer' },
+                { name = 'path' },
+              })
+            })
+          EOF
+        '';
+      }
       {
         plugin = (nvim-treesitter.withPlugins (_: pkgs.tree-sitter.allGrammars));
         config = ''
@@ -24,6 +66,56 @@
               highlight = { enable = true },
             }
           EOF
+        '';
+      }
+      {
+        plugin = nvim-lspconfig;
+        config = ''
+          lua << EOF
+          vim.cmd("nnoremap <silent> gd <cmd>lua vim.lsp.buf.definition()<CR>")
+          vim.cmd("nnoremap <silent> gD <cmd>lua vim.lsp.buf.declaration()<CR>")
+          vim.cmd("nnoremap <silent> gr <cmd>lua vim.lsp.buf.references()<CR>")
+          vim.cmd("nnoremap <silent> gi <cmd>lua vim.lsp.buf.implementation()<CR>")
+          vim.cmd('command! -nargs=0 LspVirtualTextToggle lua require("lsp/virtual_text").toggle()')
+
+          require'lspconfig'.rnix.setup{}
+          require'lspconfig'.bashls.setup{}
+          require'lspconfig'.terraform_lsp.setup{}
+          require'lspconfig'.rls.setup{}
+          require'lspconfig'.cssls.setup {}
+          require('lspconfig').yamlls.setup {
+            settings = {
+              yaml = {
+                schemas = {
+                  ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*"
+                },
+              },
+              redhat = {
+                telemetry = {
+                  disabled = false
+                }
+              },
+            }
+          }
+          EOF
+        '';
+      }
+      {
+        plugin = lspsaga-nvim;
+        config = ''
+          " show hover doc
+          nnoremap <silent>K :Lspsaga hover_doc<CR>
+          " lsp provider to find the cursor word definition and reference
+          nnoremap <silent> gh :Lspsaga lsp_finder<CR>
+          " show code actions from lsp
+          nnoremap <silent> ca :Lspsaga code_action<CR>
+          " Jump between diagnostics
+          nnoremap <silent> <C-p> :Lspsaga diagnostic_jump_prev<CR>
+          nnoremap <silent> <C-n> :Lspsaga diagnostic_jump_next<CR>
+          " scroll down hover doc or scroll in definition preview
+          nnoremap <silent> <C-f> <cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>
+          " scroll up hover doc
+          nnoremap <silent> <C-b> <cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>
         '';
       }
 
@@ -41,191 +133,13 @@
 
     extraPackages = with pkgs; [
       shellcheck
+      rnix-lsp
+      nodePackages.bash-language-server
+      terraform-lsp
+      rls
+      nodePackages.vscode-css-languageserver-bin
+      yaml-language-server
     ];
-
-    coc = with pkgs; {
-      enable = true;
-
-      settings = {
-        languageserver = {
-          nix = {
-            command = "${rnix-lsp}/bin/rnix-lsp";
-            filetypes = [ "nix" ];
-          };
-          bash = {
-            command = "${nodePackages.bash-language-server}/bin/bash-language-server";
-            args = [ "start" ];
-            filetypes = [ "sh" ];
-            ignoredRootPaths = [ "~" ];
-          };
-          terraform = {
-            command = "${terraform-lsp}/bin/terraform-lsp";
-            args = [ "serve" ];
-            filetypes = [ "terraform" "tf" ];
-          };
-          rust = {
-            command = "${rls}/bin/rls";
-            filetypes = [ "rust" ];
-            rootPatterns = [ "Cargo.toml" ".git" ];
-          };
-          css = {
-            command = "${nodePackages.vscode-css-languageserver-bin}/bin/css-languageserver";
-            args = [ "--stdio" ];
-            filetypes = [ "css" ];
-            roots = [ "package.json" ];
-          };
-          yaml = {
-            command = "${nodePackages.yaml-language-server}/bin/yaml-language-server";
-            args = [ "--stdio" ];
-            filetypes = [ "yaml" "yml" ];
-          };
-        };
-      };
-
-      pluginConfig = ''
-        " TextEdit might fail if hidden is not set.
-        set hidden
-
-        " Some servers have issues with backup files, see #649.
-        set nobackup
-        set nowritebackup
-
-        " Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable
-        " delays and poor user experience.
-        set updatetime=300
-
-        " Don't pass messages to |ins-completion-menu|.
-        set shortmess+=c
-
-        " Always show the signcolumn, otherwise it would shift the text each time
-        " diagnostics appear/become resolved.
-        if has("patch-8.1.1564")
-          " Recently vim can merge signcolumn and number column into one
-          set signcolumn=number
-        else
-          set signcolumn=yes
-        endif
-
-        " Use tab for trigger completion with characters ahead and navigate.
-        " NOTE: Use command ':verbose imap <tab>' to make sure tab is not mapped by
-        " other plugin before putting this into your config.
-        inoremap <silent><expr> <TAB>
-              \ pumvisible() ? "\<C-n>" :
-              \ <SID>check_back_space() ? "\<TAB>" :
-              \ coc#refresh()
-        inoremap <expr><S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"
-
-        function! s:check_back_space() abort
-          let col = col('.') - 1
-          return !col || getline('.')[col - 1]  =~# '\s'
-        endfunction
-
-        " Use <c-space> to trigger completion.
-        inoremap <silent><expr> <c-space> coc#refresh()
-
-        " Use <cr> to confirm completion, `<C-g>u` means break undo chain at current
-        " position. Coc only does snippet and additional edit on confirm.
-        " <cr> could be remapped by other vim plugin, try `:verbose imap <CR>`.
-        if exists('*complete_info')
-          inoremap <expr> <cr> complete_info()["selected"] != "-1" ? "\<C-y>" : "\<C-g>u\<CR>"
-        else
-          inoremap <expr> <cr> pumvisible() ? "\<C-y>" : "\<C-g>u\<CR>"
-        endif
-
-        " Use `[g` and `]g` to navigate diagnostics
-        " Use `:CocDiagnostics` to get all diagnostics of current buffer in location list.
-        nmap <silent> [g <Plug>(coc-diagnostic-prev)
-        nmap <silent> ]g <Plug>(coc-diagnostic-next)
-
-        " GoTo code navigation.
-        nmap <silent> gd <Plug>(coc-definition)
-        nmap <silent> gy <Plug>(coc-type-definition)
-        nmap <silent> gi <Plug>(coc-implementation)
-        nmap <silent> gr <Plug>(coc-references)
-
-        " Use K to show documentation in preview window.
-        nnoremap <silent> K :call <SID>show_documentation()<CR>
-
-        function! s:show_documentation()
-          if (index(['vim','help'], &filetype) >= 0)
-            execute 'h '.expand('<cword>')
-          else
-            call CocAction('doHover')
-          endif
-        endfunction
-
-        " Highlight the symbol and its references when holding the cursor.
-        autocmd CursorHold * silent call CocActionAsync('highlight')
-
-        " Symbol renaming.
-        nmap <leader>rn <Plug>(coc-rename)
-
-        " Formatting selected code.
-        xmap <leader>f  <Plug>(coc-format-selected)
-        nmap <leader>f  <Plug>(coc-format-selected)
-
-        augroup mygroup
-          autocmd!
-          " Setup formatexpr specified filetype(s).
-          autocmd FileType typescript,json setl formatexpr=CocAction('formatSelected')
-          " Update signature help on jump placeholder.
-          autocmd User CocJumpPlaceholder call CocActionAsync('showSignatureHelp')
-        augroup end
-
-        " Applying codeAction to the selected region.
-        " Example: `<leader>aap` for current paragraph
-        xmap <leader>a  <Plug>(coc-codeaction-selected)
-        nmap <leader>a  <Plug>(coc-codeaction-selected)
-
-        " Remap keys for applying codeAction to the current buffer.
-        nmap <leader>ac  <Plug>(coc-codeaction)
-        " Apply AutoFix to problem on the current line.
-        nmap <leader>qf  <Plug>(coc-fix-current)
-
-        " Map function and class text objects
-        " NOTE: Requires 'textDocument.documentSymbol' support from the language server.
-        xmap if <Plug>(coc-funcobj-i)
-        omap if <Plug>(coc-funcobj-i)
-        xmap af <Plug>(coc-funcobj-a)
-        omap af <Plug>(coc-funcobj-a)
-        xmap ic <Plug>(coc-classobj-i)
-        omap ic <Plug>(coc-classobj-i)
-        xmap ac <Plug>(coc-classobj-a)
-        omap ac <Plug>(coc-classobj-a)
-
-        " Use CTRL-S for selections ranges.
-        " Requires 'textDocument/selectionRange' support of LS, ex: coc-tsserver
-        nmap <silent> <C-s> <Plug>(coc-range-select)
-        xmap <silent> <C-s> <Plug>(coc-range-select)
-
-        " Add `:Format` command to format current buffer.
-        command! -nargs=0 Format :call CocAction('format')
-
-        " Add `:Fold` command to fold current buffer.
-        command! -nargs=? Fold :call     CocAction('fold', <f-args>)
-
-        " Add `:OR` command for organize imports of the current buffer.
-        command! -nargs=0 OR   :call     CocAction('runCommand', 'editor.action.organizeImport')
-
-        " Mappings for CoCList
-        " Show all diagnostics.
-        nnoremap <silent><nowait> <space>a  :<C-u>CocList diagnostics<cr>
-        " Manage extensions.
-        nnoremap <silent><nowait> <space>e  :<C-u>CocList extensions<cr>
-        " Show commands.
-        nnoremap <silent><nowait> <space>c  :<C-u>CocList commands<cr>
-        " Find symbol of current document.
-        nnoremap <silent><nowait> <space>o  :<C-u>CocList outline<cr>
-        " Search workspace symbols.
-        nnoremap <silent><nowait> <space>s  :<C-u>CocList -I symbols<cr>
-        " Do default action for next item.
-        nnoremap <silent><nowait> <space>j  :<C-u>CocNext<CR>
-        " Do default action for previous item.
-        nnoremap <silent><nowait> <space>k  :<C-u>CocPrev<CR>
-        " Resume latest coc list.
-        nnoremap <silent><nowait> <space>p  :<C-u>CocListResume<CR>
-      '';
-    };
 
     extraConfig = ''
       set number
