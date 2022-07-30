a9b263e1ab22e6644fc436b1ccd4e3240210a3ac
Mika Naylor <mail@autophagy.io>
2022-07-30 22:15:33 +0200

    neovim :: dont confirm cmp suggestion on enter

--- home/editors/neovim/cmp.lua
+++ home/editors/neovim/cmp.lua
@@ -15,7 +15,7 @@ cmp.setup({
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
         ['<Tab>'] = cmp.mapping.select_next_item(),
         ['<S-Tab>'] = cmp.mapping.select_prev_item(),
     }),
