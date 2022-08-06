2ac27d7a7c734f77ebac10565bd2d012efee5073
Mika Naylor <mail@autophagy.io>
2022-08-06 23:11:45 +0200

    email :: add mailcap for html & pdf

--- home/email/default.nix
+++ home/email/default.nix
@@ -1,7 +1,12 @@
-_:
+{ pkgs, ... }:
 
 {
   imports = [ ./neomutt.nix ./immerok.nix ];
+
+  home.file.".mailcap".text = ''
+    text/html; ${pkgs.w3m}/bin/w3m -I %{charset} -T text/html; copiousoutput;
+    application/pdf; ${pkgs.zathura}/bin/zathura %s
+  '';
 
   programs.mbsync.enable = true;
   programs.msmtp.enable = true;
