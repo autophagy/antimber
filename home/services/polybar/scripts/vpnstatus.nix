c40b17be5dc2ffb410e457ff76e1b92bb8951f90
Mika Naylor <mail@autophagy.io>
2022-12-26 23:33:42 +0100

    polybar :: add hostname to tailscale vpn status

--- home/services/polybar/scripts/vpnstatus.nix
+++ home/services/polybar/scripts/vpnstatus.nix
@@ -9,7 +9,8 @@ pkgs.writeShellScriptBin "vpnstatus" ''
   STATE=$(echo "$STATUS" | ${jq} -r '.BackendState')
   if [[ "$STATE" == "Running" ]]; then
       CURRENT_TAILNET=$(echo "$STATUS" | ${jq} -r '.CurrentTailnet.Name')
-      echo "VPN $CURRENT_TAILNET"
+      HOSTNAME=$(echo "$STATUS" | ${jq} -r '.Self.HostName')
+      echo "VPN $HOSTNAME@$CURRENT_TAILNET"
   else
       echo ""
   fi
