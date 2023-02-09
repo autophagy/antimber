224b74171611d50d538d5bf2e261c5727492b60b
Mika Naylor <mail@autophagy.io>
2023-02-09 21:21:51 +0100

    Don't hardcode systems, use flake utils default systems instead

--- flake.nix
+++ flake.nix
@@ -3,130 +3,121 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     nvim-scrollbar = {
       url = "github:petertriho/nvim-scrollbar";
       flake = false;
     };
-
     herbz-theme = {
       url = "https://raw.githubusercontent.com/irssi-import/themes/gh-pages/h3rbz.theme";
       flake = false;
     };
-
     nur.url = "github:nix-community/nur/master";
+    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, home-manager, nvim-scrollbar, herbz-theme, nur, ... }@inputs:
-    let
-      system = "x86_64-linux";
-      rpiSystem = "aarch64-linux";
-
-      overlays = {
-        nur = nur.overlay;
-      };
-
-      pkgs = import nixpkgs {
-        inherit system;
-        config = { allowUnfree = true; };
-        overlays = builtins.attrValues overlays;
-      };
-
-      rpiPkgs = import nixpkgs {
-        system = rpiSystem;
-      };
-
-    in
-    {
-      nvimPlugins = {
-        nvim-scrollbar = pkgs.vimUtils.buildVimPluginFrom2Nix {
-          pname = "nvim-scrollbar";
-          version = "main";
-          src = nvim-scrollbar;
+  outputs = { nixpkgs, home-manager, nvim-scrollbar, herbz-theme, nur, utils, ... }@inputs:
+    utils.lib.eachDefaultSystem (system:
+      let
+        overlays = {
+          nur = nur.overlay;
         };
-      };
 
-      # Heorot
-
-      homeConfigurations.heorot = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [
-          ./home/common/home.nix
-          ./home/heorot/home.nix
-        ];
-        extraSpecialArgs = {
-          inherit (inputs.self) nvimPlugins;
-          inherit herbz-theme;
-          rootPath = ./.;
-          hostName = "heorot";
+        pkgs = import nixpkgs {
+          inherit system;
+          config = { allowUnfree = true; };
+          overlays = builtins.attrValues overlays;
         };
-      };
 
-      nixosConfigurations.heorot = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          ./system/common/configuration.nix
-          ./system/machines/heorot
-        ];
-        specialArgs = {
-          inherit inputs;
-          hostName = "heorot";
+        nvimPlugins = {
+          nvim-scrollbar = pkgs.vimUtils.buildVimPluginFrom2Nix {
+            pname = "nvim-scrollbar";
+            version = "main";
+            src = nvim-scrollbar;
+          };
         };
-      };
+      in
+      {
+        packages = {
+          # Heorot
 
-      # Gamentol
+          homeConfigurations.heorot = home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            modules = [
+              ./home/common/home.nix
+              ./home/heorot/home.nix
+            ];
+            extraSpecialArgs = {
+              inherit nvimPlugins;
+              inherit herbz-theme;
+              rootPath = ./.;
+              hostName = "heorot";
+            };
+          };
 
-      homeConfigurations.gamentol = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [
-          ./home/common/home.nix
-          ./home/gamentol/home.nix
-        ];
-        extraSpecialArgs = {
-          inherit (inputs.self) nvimPlugins;
-          inherit herbz-theme;
-          rootPath = ./.;
-          hostName = "gamentol";
+          nixosConfigurations.heorot = nixpkgs.lib.nixosSystem {
+            inherit system;
+            modules = [
+              ./system/common/configuration.nix
+              ./system/machines/heorot
+            ];
+            specialArgs = {
+              inherit inputs;
+              hostName = "heorot";
+            };
+          };
+
+          # Gamentol
+
+          homeConfigurations.gamentol = home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            modules = [
+              ./home/common/home.nix
+              ./home/gamentol/home.nix
+            ];
+            extraSpecialArgs = {
+              inherit nvimPlugins;
+              inherit herbz-theme;
+              rootPath = ./.;
+              hostName = "gamentol";
+            };
+          };
+
+          nixosConfigurations.gamentol = nixpkgs.lib.nixosSystem {
+            inherit system;
+            modules = [
+              ./system/common/configuration.nix
+              ./system/machines/gamentol
+            ];
+            specialArgs = {
+              inherit inputs;
+              hostName = "gamentol";
+            };
+          };
+
+          # Hindberige
+
+          nixosConfigurations.hindberige = nixpkgs.lib.nixosSystem {
+            inherit system;
+            modules = [
+              ./system/machines/hindberige
+            ];
+            specialArgs = {
+              inherit inputs;
+              hostName = "hindberige";
+            };
+          };
         };
-      };
 
-      nixosConfigurations.gamentol = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          ./system/common/configuration.nix
-          ./system/machines/gamentol
-        ];
-        specialArgs = {
-          inherit inputs;
-          hostName = "gamentol";
+        devShells.ci = pkgs.mkShell {
+          buildInputs = with pkgs; [ nixpkgs-fmt statix ];
         };
-      };
 
-      # Hindberige
-
-      nixosConfigurations.hindberige = nixpkgs.lib.nixosSystem {
-        system = rpiSystem;
-        modules = [
-          ./system/machines/hindberige
-        ];
-        specialArgs = {
-          inherit inputs;
-          hostName = "hindberige";
-        };
-      };
-
-      devShells.${system}.ci = pkgs.mkShell {
-        buildInputs = with pkgs; [ nixpkgs-fmt statix ];
-      };
-
-      formatter.${system} = pkgs.nixpkgs-fmt;
-      formatter.${rpiSystem} = rpiPkgs.nixpkgs-fmt;
-    };
+        formatter = pkgs.nixpkgs-fmt;
+      });
 }
 
 
