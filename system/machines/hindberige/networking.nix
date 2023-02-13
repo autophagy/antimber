876de0065f8681f5333b63ece6353bf3b8d63dab
Mika Naylor <mail@autophagy.io>
2023-02-13 20:02:53 +0100

    hindberige :: remove tailscale from trusted interface, only open ssh/http/s

--- system/machines/hindberige/networking.nix
+++ system/machines/hindberige/networking.nix
@@ -5,9 +5,10 @@
     inherit hostName;
     firewall = {
       enable = true;
-      trustedInterfaces = [ "tailscale0" ];
       allowedTCPPorts = [
         22 # SSH
+        80 # HTTP
+        443 # HTTPS
       ];
       allowedUDPPorts = [
         41641 # Tailscale
