fcd5cabd496683a9f7f680945b347ff2c75c1175
Mika Naylor <mail@autophagy.io>
2023-02-25 19:39:45 +0000

    hindberige :: add @emilywoods user

--- system/machines/hindberige/default.nix
+++ system/machines/hindberige/default.nix
@@ -39,13 +39,21 @@
 
   users = {
     motd = builtins.readFile ./motd;
-    users.mika = {
-      isNormalUser = true;
-      extraGroups = [ "wheel" ];
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICIdbr5D6l66CFklBLi1Dl8ybAlEPu6k7Ve6B+Z+itZO mail@autophagy.io" # Heorot
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFhx/eQ3TXkbtbT//g+I29aI6iQoZA8sks5OAK/bEALP mail@autophagy.io" # Gamentol
-      ];
+    users = {
+      mika = {
+        isNormalUser = true;
+        extraGroups = [ "wheel" ];
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICIdbr5D6l66CFklBLi1Dl8ybAlEPu6k7Ve6B+Z+itZO mail@autophagy.io" # Heorot
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFhx/eQ3TXkbtbT//g+I29aI6iQoZA8sks5OAK/bEALP mail@autophagy.io" # Gamentol
+        ];
+      };
+      emily = {
+        isNormalUser = true;
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMd0dU/DlOGxmkIgbWqWuSzNPMkKUQQht4AN42Sixx0r hello@emilywoods.me"
+        ];
+      };
     };
   };
 
