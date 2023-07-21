36d55939a1d1a53c900c65c88d350a3ba12440e9
Mika Naylor <mail@autophagy.io>
2023-07-21 21:17:55 +0200

    hindberige :: add sabnzbd

--- system/machines/hindberige/default.nix
+++ system/machines/hindberige/default.nix
@@ -36,13 +36,14 @@
       trusted-public-keys = [ "autophagy.cachix.org-1:aOfvRQFCgIVXyAxlQjl3ccmlQC70X1ukzr4rDgaCMWw=" ];
     };
   };
+  nixpkgs.config.allowUnfree = true;
 
   users = {
     motd = builtins.readFile ./motd;
     users = {
       mika = {
         isNormalUser = true;
-        extraGroups = [ "wheel" ];
+        extraGroups = [ "wheel" "sabnzbd" ];
         openssh.authorizedKeys.keys = [
           "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC0EdkL/Fvhyh1G43rRVcZPZWga1m6nO6Io0G9uJE+jFb59zICccyQs6TerFckzNsVG7wssVD2M7ls2VX5ggPefDMHeWQbGuwk8DG3B5RsIAlcZHeXze2hI9LugfpDtA068QDJnGyFiLg1QnQS3hfQuC0Ra7OJ9XAb8blnF9PLvzFp1diNPUXZ1ks8udRzIl99DoLcitxLqG6R8p0WUyzVRL+EU/zL6/YbGAgoUUpTH4dAHFfe/AfPzRs4MsSwVsrnA+ohmp8sp/GNpU6tnFafeT0IkfIrfVPSfj45x+IlmlLIKi32xFLbsbUx0irynAJQ6K/DpgUqoNzCjShl/i0LT+vE/v6n8Jmqkbf5Air/g9lJ3aZeK3BISdDHeRHirU7w34r+rdVBtMyyX3E91jE2VhiuJ8GkT4jiAke/Y+bWkaY7pjv5YZMl8txyMwgQIEWGatJaTv1h4zsBNUVWavI61LXVERAI9n1FUw/4zNpZ+LExxqnbGXp0jlRGJcutXB8XQ3+Bz7uaazQNgci0hNpTIXZnlWV1eYtWl+KP39sPab1UhNd1o57Rnd35W5VXE06/Na7eTr7hcydFzWajW5JcqxOgZE8d/xta1DhL9o2RqdAcNkzNPMiFvQ8A0iiOAmVht9MpVQaBCb0jn7FUTNiKjP6ARsXwgUBMtnVxajkSozw== openpgp:0x74DABE5E"
         ];
