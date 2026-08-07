ea31da909b0eb089c55e08b10da5eab7e1a38c27
Mika Naylor <mail@autophagy.io>
2026-08-07 22:40:42 +0200

    hindberige :: try and tweak immich memory consumption

--- system/machines/hindberige/immich.nix
+++ system/machines/hindberige/immich.nix
@@ -7,6 +7,11 @@ _:
     machine-learning.enable = false;
   };
 
+  systemd.services.immich-server.environment = {
+    MALLOC_ARENA_MAX = "2";
+    VIPS_CONCURRENCY = "2";
+  };
+
   systemd.tmpfiles.rules = [
     "d /media/immich 0750 immich immich -"
   ];
